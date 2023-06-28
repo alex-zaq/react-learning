e07fe6b1cc3f4c7a6613c8b0a0749dff84d1f3bd
AlexeyZaq <melnikov1991@bk.ru>
2023-06-28 18:23:43 +0300

    step

---
+++ src/components/employers-list/employers-list.js
@@ -0,0 +1,15 @@
+import EmployersListItem from '../employers-list-item/employers-list-item'
+import './employers-list.css'
+
+
+const EmployersList = ({data}) => {
+    return (
+        <ul className="app-list list-group">
+            <EmployersListItem name="John C." salary={800}  />
+            <EmployersListItem name="Alex M." salary={3000} />
+            <EmployersListItem name="Carl W." salary={5000} />
+        </ul>
+    )
+}
+
+export default EmployersList
--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  App from './components/app/app';
 
 
-
-const text = 'Hellow World!'
-
-const elem = (
-    <div>
-            <h2>{text}</h2>
-            <input type = 'text' />
-                <button>Click</button>
-        
-    </div>   
-)
-
-// 1 во второй переводит babel
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!')
+ 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    elem
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+    // <App />
+//   <StrictMode>
+    <App />
+//   </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
