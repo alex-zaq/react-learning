ceba60443881307586b6bd9cf9bf1c80bb7dad08
AlexeyZaq <melnikov1991@bk.ru>
2023-06-30 16:47:32 +0300

    step

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,17 +4,58 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
-
-function App() {
-
-    const data = [
-        {name:'John C.', salary: 800, increase: false, id: 1},
-        {name:'Alex M.', salary: 3000, increase: true, id: 2},
-        {name:'Carl W.', salary: 15000, increase: false, id: 3},
-    ];
+import { Component } from 'react';
 
 
 
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                {name:'John C.', salary: 800, increase: false, rise: false, id: 1},
+                {name:'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name:'Carl W.', salary: 15000, increase: false, rise: false, id: 3},
+            ]
+            
+        }
+        this.maxNumber = 4
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data})=>{
+            // const index = data.findIndex(elem => elem.id === id)
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1)
+            // const newArr = [...before, ...after]
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    onToggleIncrease = (id) => {
+        console.log(`Increase this ${id}`);
+    }
+    
+    
+    onToggleRise = () => {
+        console.log(`Rise this ${id}`);
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {name, salary, increase: false, rise: false, id: this.maxNumber++}
+        this.setState(({data})=> {
+            const newArr = [...data, newItem]
+            return {
+                data: newArr
+            }
+        })
+    }
+
+
+    render () {
+        
     return (
         <div className = "app"> 
             <AppInfo />
@@ -23,11 +64,19 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployersList data={data} />
-            <EmployersAddForm />
+            <EmployersList 
+            data={this.state.data} 
+            onDelete={this.deleteItem}
+            onToggleIncrease = {this.onToggleIncrease}
+            onToggleRise = {this.onToggleRise}
+            />
+            <EmployersAddForm onAdd={this.addItem} />
         </div>
        
     )
+    }
 }
 
+
+ 
 export default App
